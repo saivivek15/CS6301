6787d420b69768b15e2f12fd7b156186a7b6f191
saivivek15 <sai.vivek15@gmail.com>
2017-09-03 15:57:10 -0500

    Diamter updated with Parent nodes

--- Diameter.java
+++ Diameter.java
@@ -2,80 +2,76 @@ package cs6301.g33;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 /*
  * @author Sai Vivek Kanaparthy
+ * @author Chandra Sekhar Guntupalli
+ * @author Sushma Eati
+ * @author Abhinaya Krishna Mandepudi
  */
 
 
 public class Diameter {
-	
-	public static LinkedList<Graph.Vertex> path, tmp;
-    public static int[] previous, distance;
-    CCVertex[] ccVertex;
+	public static Graph.Vertex pathLastVertex; // Last Vertex of BFS traversal
+	public static LinkedList<Graph.Vertex> path; // Queue maintained in a BFS traversal
+    public static int[] distance; // Distance from the start node of the traversal
+    public static DVertex[] dVertex; 
     Graph g;
     
     
     // Class to store information about a vertex in this algorithm
-    class CCVertex {
+    class DVertex {
     	Graph.Vertex element;
     	boolean seen;
-    	int cno;
-    	CCVertex(Graph.Vertex u) {
+    	Graph.Vertex parent;
+    	DVertex(Graph.Vertex u) {
     		element = u;
     		seen = false;
-    		cno = -1;
     	}
     }
     
     public Diameter(Graph g) {
     	this.g = g;
-    	ccVertex = new CCVertex[g.size()];
-    	for(Graph.Vertex u: g) { ccVertex[u.name] = new CCVertex(u); }
+    	dVertex = new DVertex[g.size()];
+    	for(Graph.Vertex u: g) { dVertex[u.name] = new DVertex(u); }
     }
     
     
     /**
-	 * @param u : vertex from where BFS is executed
+	 * @param u : start vertex from where BFS is executed
 	 * Procedure for breadth first search algorithm 
 	 */
     public  void bfsVisit(Graph.Vertex u) {
-    	// path is used to store the order of traversal and can be accesed anywhere as it static variable
-    	path = new LinkedList<>();
+    	
     	//Queue for traversing 
-    	tmp  = new LinkedList<>();
-    	// parent node of the node
-    	previous = new int[g.n];
+    	path  = new LinkedList<>();
     	// distance of node from the start node
     	distance = new int[g.n];
+    	//initialized with -1
     	for(int i=0;i<distance.length;i++){
     		distance[i]=-1;
-    		previous[i]=u.getName();
     	}
     	visit(u);
     	path.add(u);
-    	tmp.add(u);
+    	pathLastVertex=u;
     	distance[u.getName()]=0;
-    	while(tmp.size()!=0){
+    	while(path.size()!=0){
     		// remove the visited node 
-    		Graph.Vertex start = tmp.remove();
+    		Graph.Vertex start = path.remove();
+    		pathLastVertex = start;
     		// traverse across the adjacency list of the node
-    		Iterator<Graph.Edge> it = start.adj.iterator();
-    		while(it.hasNext()){
-    		Graph.Edge e = it.next();
-    		Graph.Vertex v = e.otherEnd(start);
-    		if(distance[v.getName()]==-1){
-    			distance[v.getName()] = distance[start.getName()]+1;
-    			previous[v.getName()] = start.getName();
-    			}
-    		if(!seen(v)) {
-    			visit(v);
-    			path.add(v);
-    			tmp.add(v);
-		    	}	
+    		for(Graph.Edge e: start.adj){
+        		Graph.Vertex v = e.otherEnd(start);
+        		if(distance[v.getName()]==-1){
+        			distance[v.getName()] = distance[start.getName()]+1;
+        			dVertex[v.getName()].parent = start;
+        			}
+        		if(!seen(v)) {
+        			visit(v);
+        			path.add(v);
+    		    	}	
     		}
     	}
     }
@@ -86,39 +82,39 @@ public class Diameter {
      * @param g : Graph on which the diameter is to be determined
      * @return LinkedList of path of vertices of diameter
      */
-    public LinkedList<Graph.Vertex> diameter(Graph g){
+    public static LinkedList<Graph.Vertex> diameter(Graph g){
     	Diameter bfsOne = new Diameter(g);
     	LinkedList<Graph.Vertex> diameterPath = new LinkedList<Graph.Vertex>();
     	// First BFS call with arbitrary start node
-    	bfsOne.bfsVisit(g.getVertex(2));
-    	Graph.Vertex lastBFSOne = path.getLast();
+    	bfsOne.bfsVisit(g.v[2]);
+    	Graph.Vertex lastBFSOne = pathLastVertex;
     	Diameter bfsTwo = new Diameter(g);
     	// Second BFS call with end node of first BFS
     	bfsTwo.bfsVisit(lastBFSOne);
-    	Graph.Vertex lastBFSTwo =path.getLast();
+    	Graph.Vertex lastBFSTwo =pathLastVertex;
     	while(lastBFSTwo!=lastBFSOne){
     		diameterPath.add(lastBFSTwo);
-    		int pr = previous[lastBFSTwo.getName()]+1;
-    		lastBFSTwo = g.getVertex(pr);
+    		Graph.Vertex pr1 = dVertex[lastBFSTwo.getName()].parent;
+    		lastBFSTwo = pr1;
     	}
     	diameterPath.add(lastBFSTwo);
     	return diameterPath;
     }
     
     public boolean seen(Graph.Vertex u) {
-		CCVertex ccu = getCCVertex(u);
+		DVertex ccu = getDVertex(u);
 		return ccu.seen;
     }
 
     // Visit a node by marking it as seen
     void visit(Graph.Vertex u) {
-    	CCVertex ccu = getCCVertex(u);
+    	DVertex ccu = getDVertex(u);
     	ccu.seen = true;
     }
 
     // From Vertex to CCVertex 
-    CCVertex getCCVertex(Graph.Vertex u) {
-	return ccVertex[u.name];
+    DVertex getDVertex(Graph.Vertex u) {
+	return dVertex[u.name];
     }
     
     
@@ -135,8 +131,7 @@ public class Diameter {
             in = new Scanner(System.in);
         }
 	Graph g = Graph.readGraph(in);
-	Diameter d = new Diameter(g);
-	LinkedList<Graph.Vertex> dmtrPath=d.diameter(g);
+	LinkedList<Graph.Vertex> dmtrPath=diameter(g);
 	System.out.println("Length of diameter: "+dmtrPath.size());
 	System.out.print("Path of diameter: ");
 	for(Graph.Vertex vertex: dmtrPath){
