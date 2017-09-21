7694a34d88e99f81da24f25b98f57484d1d25055
saivivek15 <sai.vivek15@gmail.com>
2017-09-20 22:31:06 -0500

    Level 1 and karatsuba

--- longProject1/LP1L1.java
+++ longProject1/LP1L1.java
@@ -5,15 +5,20 @@ package cs6301.g33.longProject1;
 
 public class LP1L1 {
     public static void main(String[] args) {
-	Num x = new Num(54321);
-	Num y = new Num(4321);
+	Num x = new Num(99999999);
+	Num y = new Num(99);
 	Num z = Num.add(x, y);
-	System.out.println(x);
-	System.out.println(y);
-	System.out.println(z);
-	
-	//Num a = Num.power(x, 8);
-	//System.out.println(z);
-	z.printList();
+	Num w = Num.subtract(x, y);
+	Num u = Num.product(x, y);
+	Num v = Num.karatsuba(x, y);
+	Num a = Num.power(x, 2);
+	System.out.println("x: "+x);
+	System.out.println("y: "+y);
+	System.out.println("z: "+z);
+	System.out.println("w: "+w);
+	System.out.println("u: "+u);
+	System.out.println("a: "+a);
+	System.out.println("v: "+v);
+	w.printList();
     }
 }
--- longProject1/Num.java
+++ longProject1/Num.java
@@ -1,9 +1,9 @@
-
-// Starter code for lp1.
-
-// Change following line to your group number
-// Changed type of base to long: 1:15 PM, 2017-09-08.
 package cs6301.g33.longProject1;
+
+/**
+ * @author Sai Vivek Kanaparthy
+ * @author Sushma Eati
+ */
 
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -19,7 +19,6 @@ public class Num  implements Comparable<Num> {
     	for(int i=0;i<size;i++){
     		this.numObj.add((long) (s.charAt(size-i-1)-'0'));
     	}
-
     }
 
     Num(long x) {
@@ -67,18 +66,164 @@ public class Num  implements Comparable<Num> {
     }
 
     static Num subtract(Num a, Num b) {
-    	
-	return null;
+    	long carry=0;
+    	Iterator<Long> it1 = a.numObj.iterator();
+    	Iterator<Long> it2 = b.numObj.iterator();
+    	LinkedList<Long> numObj = new LinkedList<>(); 
+    	int cmp = a.compareTo(b);
+    	if(cmp == 0){
+    		return new Num(0);
+    	}
+    	else if(cmp > 0){
+    		while(it1.hasNext() && it2.hasNext()){
+        		long temp=0;
+        		temp = it1.next()-it2.next()-carry;
+        		carry =0;
+        		if (temp <0){
+        			temp = temp+base;
+        			carry =1;
+        		}
+        		numObj.add(temp);
+    		}
+    		while(it1.hasNext()){
+        		long temp =0;
+        		temp = it1.next()-carry;
+        		carry = 0;
+        		if(temp<0){
+        			temp = temp+base;
+        			carry=1;
+        		}
+        		numObj.add(temp);
+        	}
+    		Num sub = new Num(0);
+    	    sub.numObj=numObj;
+    		return sub;
+    	}
+    	else{
+    		while(it1.hasNext() && it2.hasNext()){
+        		long temp=0;
+        		temp = it2.next()-it1.next()-carry;
+        		carry =0;
+        		if (temp <0){
+        			temp = temp+base;
+        			carry =1;
+        		}
+        		numObj.add(temp);
+    		}
+    		while(it2.hasNext()){
+        		long temp =0;
+        		temp = it2.next()-carry;
+        		carry = 0;
+        		if(temp<0){
+        			temp = temp+base;
+        			carry=1;
+        		}
+        		numObj.add(temp);
+        	}
+    		Num sub = new Num(0);
+    	    sub.numObj=numObj;
+    		return sub;
+    	}
     }
+    
+    
+    static Num product(Num a, Num b){
+    	long finalSize = a.numObj.size()+b.numObj.size()+1;
+    	Num result = new Num(0);
+    	for(int i=0;i<=finalSize;i++){
+        	result.numObj.add((long)0);
+    	}
 
+    	for(int i=0;i<a.numObj.size();i++){
+    		long carry=0;
+    		LinkedList<Long> tmpProduct = new LinkedList<>();
+        	for(int each=0;each<=finalSize;each++){
+            	tmpProduct.add((long)0);
+        	}
+    		for(int j=0;j<b.numObj.size();j++){
+    			long temp=a.numObj.get(i)*b.numObj.get(j)+carry;
+    			carry = temp/base;
+    			tmpProduct.set(j,temp % base);
+    		}
+    		long carryResult=0;
+    		int j=0;
+    		for(j=0;j<b.numObj.size()+1;j++){
+    			long temp = result.numObj.get(i+j)+tmpProduct.get(j)+carryResult;
+    			carryResult = temp/base;
+    			result.numObj.set(i+j, temp % base);
+    		}
+    		if(carry !=0){
+    			long temp = result.numObj.get(i+j-1);
+    			result.numObj.set(i+j-1,temp+carry);
+    		}
+    	}
+
+    	return result;
+    	
+    }
     // Implement Karatsuba algorithm for excellence credit
-    static Num product(Num a, Num b) {
-	return null;
+    static Num karatsuba(Num a, Num b) {
+    	long baseSize;
+    	int cmp = a.compareTo(b);
+    	if(cmp >1){
+    		baseSize = a.numObj.size();
+    	}else{
+    		baseSize = b.numObj.size();
+    	}
+    	if(baseSize<3){
+    		return product(a, b);
+    	}
+    	baseSize = (baseSize/2);
+    	baseSize += (baseSize % 2);
+    	Num aLow = new Num(0);
+    	Num aHigh = new Num(0);
+    	Num bLow = new Num(0);
+    	Num bHigh = new Num(0);
+    	long count =0;
+    	for(Long ita:a.numObj){
+    		count +=1;
+    		if(count>=baseSize){
+    			aHigh.numObj.add(ita);
+    		}
+    		aLow.numObj.add(ita);
+    	}
+    	count=0;
+    	for(Long itb:b.numObj){
+    		count +=1;
+    		if(count>=baseSize){
+    			bHigh.numObj.add(itb);
+    		}
+    		bLow.numObj.add(itb);
+    	}
+    	Num aLowbLow = karatsuba(aLow,bLow);
+    	Num aHighbHigh = karatsuba(aHigh, bHigh);
+    	Num ab = karatsuba(add(aLow, bHigh), add(aHigh,bLow));
+    	for(long i=0;i<2*baseSize;i++){
+    		aHighbHigh.numObj.add((long) 0);
+    	}
+    	for(long i=0;i<baseSize;i++){
+    		ab.numObj.add((long) 0);
+    	}
+    	Num result = Num.add(aLowbLow, ab);
+    	result = Num.add(aHighbHigh, result);
+	return result;
     }
 
     // Use divide and conquer
     static Num power(Num a, long n) {
-	return null;
+    	if(n ==0){
+    		return new Num(1); 
+    	}else if(n==1){
+    		return a;
+    	}else{
+    		Num S = power(a,n/2);
+    		if(n%2==0){
+    			return product(S,S);
+    		}else{
+    			S = product(S, S);
+    			return product(S,a);
+    		}
+    	}
     }
     /* End of Level 1 */
 
@@ -88,7 +233,10 @@ public class Num  implements Comparable<Num> {
     }
 
     static Num mod(Num a, Num b) {
-	return null;
+    	Num tmp = divide(a, b);
+    	tmp = product(tmp, b);
+    	tmp = subtract(a, tmp);
+	return tmp;
     }
 
     // Use divide and conquer
@@ -150,12 +298,10 @@ public class Num  implements Comparable<Num> {
     	if(this.numObj.size()==0){
     		return number;
     	}
-    	long last = it.next();
-    	if(last!=0){
-    		number += last;
-    	}
-    	while(it.hasNext())
+    	while(it.hasNext()){
     		number += it.next();
+    	}
+    	number = number.replaceFirst("^0+","");
 	return number;
     }
 
